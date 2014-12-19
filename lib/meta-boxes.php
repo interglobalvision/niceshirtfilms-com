ebca756625fce356c0d95b081a4dc46d5fb8c38a
Michael Ray-Von <mikey@migey.local>
2014-12-18 20:50:16 -0600

    adds custom metaboxes for Director and Showreels

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -1,9 +1,9 @@
 <?php
 
 /* Get post objects for select field options */ 
-function get_post_objects( $query_args ) {
+function get_director_objects( $query_args ) {
 $args = wp_parse_args( $query_args, array(
-    'post_type' => 'post',
+    'post_type' => 'director',
 ) );
 $posts = get_posts( $args );
 $post_options = array();
@@ -13,6 +13,21 @@ if ( $posts ) {
     }
 }
 return $post_options;
+}
+
+function get_video_objects( $query_args ) {
+$args = wp_parse_args( $query_args, array(
+    'post_type' => 'video',
+) );
+$posts = get_posts( $args );
+$post_options = array();
+if ( $posts ) {
+    foreach ( $posts as $post ) {
+        $post_options [ $post->ID ] = $post->post_title;
+    }
+}
+return $post_options;
+
 }
 
 
@@ -30,7 +45,120 @@ function cmb_sample_metaboxes( array $meta_boxes ) {
 
 	$prefix = '_igv_';
 
-	//Metaboxes
+	$meta_boxes['director_metabox'] = array(
+		'id'         => 'director_metabox',
+		'title'      => __( 'Director', 'cmb' ),
+		'pages'      => array( 'video','showreel' ), // Post type
+		'context'    => 'normal',
+		'priority'   => 'high',
+		'show_names' => false, // Show field names on the left
+		// 'cmb_styles' => true, // Enqueue the CMB stylesheet on the frontend
+		'fields'     => array(
+			array(
+				'name'    => __( 'Director', 'cmb' ),
+				'desc'    => __( '', 'cmb' ),
+				'id'      => $prefix . 'director',
+				'type'    => 'select',
+				'options' => get_director_objects(),
+			),
+		)
+	);
+
+	$meta_boxes['showreel_1_metabox'] = array(
+		'id'         => 'showreel_1_metabox',
+		'title'      => __( 'Showreel 1', 'cmb' ),
+		'pages'      => array( 'director', ), // Post type
+		'context'    => 'normal',
+		'priority'   => 'high',
+		'show_names' => true, // Show field names on the left
+		// 'cmb_styles' => true, // Enqueue the CMB stylesheet on the frontend
+		'fields'     => array(
+			array(
+				'id'          => 'showreel_1',
+				'type'        => 'group',
+				'description' => __( '', 'cmb' ),
+				'options'     => array(
+					'group_title'   => __( 'Video {#}', 'cmb' ), // {#} gets replaced by row number
+					'add_button'    => __( 'Add Another Video', 'cmb' ),
+					'remove_button' => __( 'Remove Video', 'cmb' ),
+					'sortable'      => true, // beta
+				),
+				'fields'      => array(
+					array(
+						'name'    => __( 'Video', 'cmb' ),
+						'desc'    => __( '', 'cmb' ),
+						'id'      => 'video',
+						'type'    => 'select',
+						'options' => get_video_objects(),
+					),
+				),
+			),
+		),
+	);
+
+	$meta_boxes['showreel_2_metabox'] = array(
+		'id'         => 'showreel_2_metabox',
+		'title'      => __( 'Showreel 2', 'cmb' ),
+		'pages'      => array( 'director', ), // Post type
+		'context'    => 'normal',
+		'priority'   => 'high',
+		'show_names' => true, // Show field names on the left
+		// 'cmb_styles' => true, // Enqueue the CMB stylesheet on the frontend
+		'fields'     => array(
+			array(
+				'id'          => 'showreel_2',
+				'type'        => 'group',
+				'description' => __( '', 'cmb' ),
+				'options'     => array(
+					'group_title'   => __( 'Video {#}', 'cmb' ), // {#} gets replaced by row number
+					'add_button'    => __( 'Add Another Video', 'cmb' ),
+					'remove_button' => __( 'Remove Video', 'cmb' ),
+					'sortable'      => true, // beta
+				),
+				'fields'      => array(
+					array(
+						'name'    => __( 'Video', 'cmb' ),
+						'desc'    => __( '', 'cmb' ),
+						'id'      => 'video',
+						'type'    => 'select',
+						'options' => get_video_objects(),
+					),
+				),
+			),
+		),
+	);
+
+	$meta_boxes['showreel_3_metabox'] = array(
+		'id'         => 'showreel_3_metabox',
+		'title'      => __( 'Showreel 3', 'cmb' ),
+		'pages'      => array( 'director', ), // Post type
+		'context'    => 'normal',
+		'priority'   => 'high',
+		'show_names' => true, // Show field names on the left
+		// 'cmb_styles' => true, // Enqueue the CMB stylesheet on the frontend
+		'fields'     => array(
+			array(
+				'id'          => 'showreel_3',
+				'type'        => 'group',
+				'description' => __( '', 'cmb' ),
+				'options'     => array(
+					'group_title'   => __( 'Video {#}', 'cmb' ), // {#} gets replaced by row number
+					'add_button'    => __( 'Add Another Video', 'cmb' ),
+					'remove_button' => __( 'Remove Video', 'cmb' ),
+					'sortable'      => true, // beta
+				),
+				'fields'      => array(
+					array(
+						'name'    => __( 'Video', 'cmb' ),
+						'desc'    => __( '', 'cmb' ),
+						'id'      => 'video',
+						'type'    => 'select',
+						'options' => get_video_objects(),
+					),
+				),
+			),
+		),
+	);
 
 	return $meta_boxes;
 }
