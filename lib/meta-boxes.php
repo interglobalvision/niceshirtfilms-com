c9e1b764ee00868d299c374881fc6000a27f62a2
cas <casska@gmail.com>
2015-02-20 16:26:22 -0600

    update - fields declaration

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -25,9 +25,9 @@ return $post_options;
  * @license  http://www.opensource.org/licenses/gpl-license.php GPL v2.0 (or later)
  * @link     https://github.com/webdevstudios/Custom-Metaboxes-and-Fields-for-WordPress
  */
-add_filter( 'cmb_meta_boxes', 'cmb_sample_metaboxes' );
+add_action( 'cmb2_init', 'cmb_sample_metaboxes' );
 
-function cmb_sample_metaboxes( array $meta_boxes ) {
+function cmb_sample_metaboxes() {
 
 	if (isset($_GET['post'])) {
 		$post_ID = $_GET['post'];
@@ -53,172 +53,115 @@ function cmb_sample_metaboxes( array $meta_boxes ) {
 		)
   );
 
-	$meta_boxes['posts_metabox'] = array(
+  // Add metabox: Post Options
+	$posts_metabox = new_cmb2_box( array(
 		'id'         => 'post_metabox',
 		'title'      => __( 'Post Options', 'cmb' ),
-		'pages'      => array( 'post' ), // Post type
+		'object_types'      => array( 'post' ), // Post type
 		'context'    => 'normal',
 		'priority'   => 'high',
 		'show_names' => true, // Show field names on the left
-		'fields'     => array(
-  		  array(
-          'name' => 'Long title',
-          'desc' => 'longer title that also allows formatting',
-          'id' => $prefix . 'longtitle',
-          'type' => 'wysiwyg'
-        ),
-  		  array(
-          'name' => 'Read More post?',
-          'desc' => 'show read more',
-          'id' => $prefix . 'readmore',
-          'type' => 'checkbox'
-        ),
-  		  array(
-          'name' => 'Thumbnail background?',
-          'desc' => 'shows thumbnail as title background',
-          'id' => $prefix . 'thumbbackground',
-          'type' => 'checkbox'
-        ),
-        array(
-            'name' => 'Background Color Picker',
-            'id'   => $prefix . 'color',
-            'type' => 'colorpicker',
-            'default'  => '#000000',
-            'repeatable' => false,
-        ),
-		)
-	);
+  ));
+  
+  // Add field: Long Title
+  $posts_metabox->add_field( array(
+    'name' => 'Long title',
+    'desc' => 'longer title that also allows formatting',
+    'id' => $prefix . 'longtitle',
+    'type' => 'wysiwyg'
+  ) );
+  
+  // Add field: Read More?
+  $posts_metabox->add_field( array(
+    'name' => 'Read More post?',
+    'desc' => 'show read more',
+    'id' => $prefix . 'readmore',
+    'type' => 'checkbox'
+  ) );
+  
+  // Add field: Thumbnail Background
+  $posts_metabox->add_field( array(
+    'name' => 'Thumbnail background?',
+    'desc' => 'shows thumbnail as title background',
+    'id' => $prefix . 'thumbbackground',
+    'type' => 'checkbox'
+  ) );
+  
+  // Add field: Background Color
+  $posts_metabox->add_field( array(
+    'name' => 'Background Color Picker',
+    'id'   => $prefix . 'color',
+    'type' => 'colorpicker',
+    'default'  => '#000000',
+    'repeatable' => false,
+  ) );
 
-	$meta_boxes['director_metabox'] = array(
+  // Add metabox: Director
+	$director_metabox = new_cmb2_box( array(
 		'id'         => 'director_metabox',
 		'title'      => __( 'Director', 'cmb' ),
-		'pages'      => array( 'video' ), // Post type
+		'object_types'      => array( 'video' ), // Post type
 		'context'    => 'normal',
 		'priority'   => 'high',
 		'show_names' => true, // Show field names on the left
-		'fields'     => array(
-			array(
-				'name'    => __( 'Title', 'cmb' ),
-				'desc'    => __( '', 'cmb' ),
-				'id'      => $prefix . 'title',
-				'type'    => 'text',
-			),
-			array(
-				'name'    => __( 'Brand', 'cmb' ),
-				'desc'    => __( '', 'cmb' ),
-				'id'      => $prefix . 'brand',
-				'type'    => 'text',
-			),
-			array(
-				'name'    => __( 'Director', 'cmb' ),
-				'desc'    => __( '', 'cmb' ),
-				'id'      => $prefix . 'director',
-				'type'    => 'select',
-				'options' => get_post_objects($director_args),
-			),
-		)
-	);
+  ) );
+  
+  // Add field: Title
+  $director_metabox->add_field( array(
+    'name'    => __( 'Title', 'cmb' ),
+    'desc'    => __( '', 'cmb' ),
+    'id'      => $prefix . 'title',
+    'type'    => 'text',
+  ) );
+  
+  // Add field: Brand
+  $director_metabox->add_field( array(
+    'name'    => __( 'Brand', 'cmb' ),
+    'desc'    => __( '', 'cmb' ),
+    'id'      => $prefix . 'brand',
+    'type'    => 'text',
+  ) );
+  
+  // Add field: Director
+  $director_metabox->add_field( array(
+    'name'    => __( 'Director', 'cmb' ),
+    'desc'    => __( '', 'cmb' ),
+    'id'      => $prefix . 'director',
+    'type'    => 'select',
+    'options' => get_post_objects($director_args),
+  ) );
 
-	$meta_boxes['showreel_1_metabox'] = array(
-		'id'         => 'showreel_1_metabox',
-		'title'      => __( 'Showreel 1', 'cmb' ),
-		'pages'      => array( 'director', ), // Post type
-		'context'    => 'normal',
-		'priority'   => 'high',
-		'show_names' => true, // Show field names on the left
-		// 'cmb_styles' => true, // Enqueue the CMB stylesheet on the frontend
-		'fields'     => array(
-			array(
-				'id'          => $prefix . 'showreel_1',
-				'type'        => 'group',
-				'description' => __( '', 'cmb' ),
-				'options'     => array(
-					'group_title'   => __( 'Video {#}', 'cmb' ), // {#} gets replaced by row number
-					'add_button'    => __( 'Add Another Video', 'cmb' ),
-					'remove_button' => __( 'Remove Video', 'cmb' ),
-					'sortable'      => true, // beta
-				),
-				'fields'      => array(
-					array(
-						'name'    => __( 'Video', 'cmb' ),
-						'desc'    => __( '', 'cmb' ),
-						'id'      => 'video',
-						'type'    => 'select',
-						'options' => get_post_objects($showreel_args),
-					),
-				),
-			),
-		),
-	);
+  // Add metabox: Showreel
+  $showreel_1_metabox = new_cmb2_box( array(
+    'id'         => 'showreel_1_metabox',
+    'title'      => __( 'Showreel 1', 'cmb' ),
+    'object_types'      => array( 'director', ), // Post type
+    'context'    => 'normal',
+    'priority'   => 'high',
+    'show_names' => true, // Show field names on the left
+    // 'cmb_styles' => true, // Enqueue the CMB stylesheet on the frontend
+  ) );
 
-	/*
-$meta_boxes['showreel_2_metabox'] = array(
-		'id'         => 'showreel_2_metabox',
-		'title'      => __( 'Showreel 2', 'cmb' ),
-		'pages'      => array( 'director', ), // Post type
-		'context'    => 'normal',
-		'priority'   => 'high',
-		'show_names' => true, // Show field names on the left
-		// 'cmb_styles' => true, // Enqueue the CMB stylesheet on the frontend
-		'fields'     => array(
-			array(
-				'id'          => $prefix . 'showreel_2',
-				'type'        => 'group',
-				'description' => __( '', 'cmb' ),
-				'options'     => array(
-					'group_title'   => __( 'Video {#}', 'cmb' ), // {#} gets replaced by row number
-					'add_button'    => __( 'Add Another Video', 'cmb' ),
-					'remove_button' => __( 'Remove Video', 'cmb' ),
-					'sortable'      => true, // beta
-				),
-				'fields'      => array(
-					array(
-						'name'    => __( 'Video', 'cmb' ),
-						'desc'    => __( '', 'cmb' ),
-						'id'      => 'video',
-						'type'    => 'select',
-						'options' => get_post_objects($showreel_args),
-					),
-				),
-			),
-		),
-	);
-*/
-
-	/*
-$meta_boxes['showreel_3_metabox'] = array(
-		'id'         => 'showreel_3_metabox',
-		'title'      => __( 'Showreel 3', 'cmb' ),
-		'pages'      => array( 'director', ), // Post type
-		'context'    => 'normal',
-		'priority'   => 'high',
-		'show_names' => true, // Show field names on the left
-		// 'cmb_styles' => true, // Enqueue the CMB stylesheet on the frontend
-		'fields'     => array(
-			array(
-				'id'          => $prefix . 'showreel_3',
-				'type'        => 'group',
-				'description' => __( '', 'cmb' ),
-				'options'     => array(
-					'group_title'   => __( 'Video {#}', 'cmb' ), // {#} gets replaced by row number
-					'add_button'    => __( 'Add Another Video', 'cmb' ),
-					'remove_button' => __( 'Remove Video', 'cmb' ),
-					'sortable'      => true, // beta
-				),
-				'fields'      => array(
-					array(
-						'name'    => __( 'Video', 'cmb' ),
-						'desc'    => __( '', 'cmb' ),
-						'id'      => 'video',
-						'type'    => 'select',
-						'options' => get_post_objects($showreel_args),
-					),
-				),
-			),
-		),
-	);
-*/
-
-	return $meta_boxes;
+  // Add field: Group Showreel
+  $showreel_1_metabox->add_field( array(
+    'id'          => $prefix . 'showreel_1',
+    'type'        => 'group',
+    'description' => __( '', 'cmb' ),
+    'options'     => array(
+      'group_title'   => __( 'Video {#}', 'cmb' ), // {#} gets replaced by row number
+      'add_button'    => __( 'Add Another Video', 'cmb' ),
+      'remove_button' => __( 'Remove Video', 'cmb' ),
+      'sortable'      => true, // beta
+    )
+  ) );
+  
+  // Add field: Video
+  $showreel_1_metabox->add_field( array(
+    'name'    => __( 'Video', 'cmb' ),
+    'desc'    => __( '', 'cmb' ),
+    'id'      => 'video',
+    'type'    => 'select',
+    'options' => get_post_objects($showreel_args),
+  ) );
 }
 ?>
