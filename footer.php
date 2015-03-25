e7cfd49a0caef0ec4c74ada0805f99038c4a067c
Patrick Best <patrickbest@patrickbest.com>
2015-03-25 14:26:23 -0600

    add google analytics

--- footer.php
+++ footer.php
@@ -30,6 +30,17 @@
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA1dtJVKkbFoIGINO734bpSLHV4fg1mYKg"></script>
     <script src="//f.vimeocdn.com/js/froogaloop2.min.js"></script>
     <?php wp_footer(); ?>
+
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-8386387-1']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
   </section>
   </body>
 </html>
