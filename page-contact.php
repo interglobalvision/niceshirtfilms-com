dc24d6d738939cd87eb69872f65695cd9e853f8f
MattAppleton <matt@modernactivity.com>
2017-09-28 20:27:16 +0100

    simplify contact page
    removed shirts and floated contact page blocks, temp client request... have already done this on live site! Matt x

--- page-contact.php
+++ page-contact.php
@@ -27,10 +27,11 @@ if( have_posts() ) {
 <?php
 $people = get_posts('post_type=people&posts_per_page=-1&orderby=menu_order');
 foreach ($people as $post) {
-  $img = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'width-250');
+//   $img = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'width-250');
   $meta = get_post_meta($post->ID);
 ?>
-      <div class="contact-person row u-cf">
+      <div class="contact-person u-cf">
+<!--
         <div class="col col1 u-align-right">
           <img src="<?php echo $img[0]; ?>" />
 <?php
@@ -38,16 +39,23 @@ if (!$img) {
   echo '&nbsp;';
 } ?>
         </div>
+-->
 
-        <div class="col col2">
-<?php
+        <div class="">
+<h3><?php
+	
+
   echo $post->post_title;
   echo '<br/>';
+  
+?></h3>
 
+<?php
+        
   if (!empty($meta['_igv_title'][0])) {
     echo $meta['_igv_title'][0];
     echo '<br/>';
-  }
+  } 
 
   if (!empty($meta['_igv_phone'][0])) {
     echo $meta['_igv_phone'][0];
